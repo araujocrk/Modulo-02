57d97048cfd6ba8e3a9d38b563e14acaceef5b11
araujocrk <ryansaopaulo2020@outlook.com>
2024-10-17 11:42:48 -0300

    Create Pedra-papel-tesoura.py

---
+++ Programacao_Orientada_a_Objetos/Atividades/Pedra-papel-tesoura.py
@@ -0,0 +1,47 @@
+import random
+class Jogo:
+    escolhaComputador = None
+    escolhaUsuario = None
+    opcoes = ['pedra', 'papel', 'tesoura']
+    
+    def obterEscolhaUsuario(self):
+        self.escolhaUsuario = input('Escolha Pedra, Papel ou Tesoura: ').strip().lower()
+        if self.escolhaUsuario in self.opcoes:
+            return self.escolhaUsuario
+        else:
+            print('Escolha inválida. Tente novamente!')
+            return self.obterEscolhaUsuario()
+        
+    def obterEscolhaComputador(self):
+        self.escolhaComputador = random.choice(self.opcoes)
+        return self.escolhaComputador
+
+    def obterResultado(self):
+        if self.escolhaComputador == self.escolhaUsuario:
+            return 'Empate!'
+        elif (self.escolhaUsuario == 'pedra' and self.escolhaComputador == 'tesoura') or \
+             (self.escolhaUsuario == 'papel' and self.escolhaComputador == 'pedra') or \
+             (self.escolhaUsuario == 'tesoura' and self.escolhaComputador == 'papel'):
+            return 'Você venceu!'
+        else:
+            return 'Você perdeu!'    
+
+def main():
+    print('Bem-vindo ao jogo Pedra, Papel e Tesoura!')
+    meuJogo = Jogo()
+    while True:
+        meuJogo.obterEscolhaUsuario()
+        meuJogo.obterEscolhaComputador()
+        print(f'Computador escolheu {meuJogo.escolhaComputador}')
+        print(meuJogo.obterResultado())
+        vaiContinuar = input('Jogar Novamente? (S/N) ').upper()
+        if vaiContinuar[0] == 'N':
+            print('Encerrando o jogo...')
+            break
+        elif vaiContinuar[0] == 'S':
+            pass
+        else:
+            print('Opção inválida! Jogo reiniciado.')
+
+if __name__ == '__main__':
+    main()
