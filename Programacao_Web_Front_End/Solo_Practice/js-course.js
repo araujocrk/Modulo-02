fdfc6d209781a2189f9462aae597b5f7de02c9f8
araujocrk <ryansaopaulo2020@outlook.com>
2024-10-23 12:05:24 -0300

    continue practice

---
+++ Programacao_Web_Front_End/Solo_Practice/js-course.js
@@ -0,0 +1,50 @@
+// Sum (+)
+var sum = 5 + 5;
+
+// Subtraction (-)
+var difference = 10 - 5;
+
+// Multiplication (*)
+var product = 10 * 5;
+
+// Division (/)
+var quotient = 10 / 5;
+
+// Remainder (%)
+var remainder = 12 % 5;
+
+// Exponentiation  (**)
+var exponentiation  = 10 ** 2;
+
+// Incrementing number (++) ps: ++ = 1
+var increment = 10;
+increment++;
+
+// Decrementing number (--) ps: -- = -1
+var decrement = 10;
+decrement--;
+
+// Decimal/Float numbers (.)
+var decimal = 10.5;
+
+// Compound Assignment (+=, -=, *=, /=, %=)
+var compoundPlus = 10, compoundMinus = 20, compoundMultiply = 5, compoundDivide = 30, compoundRemainder = 10;
+compoundPlus += 10;
+compoundMinus -= 10;
+compoundMultiply *= 10;
+compoundDivide /= 5
+compoundRemainder %= 4;
+
+// Create a variable with string value
+var myFirstName = 'Ryan', myLastName = 'Araújo';
+
+// Escape Character (\) or Use single quotes ('') or Use a backticks (`) to use both of them at the same time
+var myStr = "I'm a \"double-quoted\" string.";
+myStr = 'I\'m a "double-quoted" string.';
+myStr = `I'm a "double-quoted" string.`;
+console.log(myStr);
+// All escape characters 
+/*
+
+*/
+
