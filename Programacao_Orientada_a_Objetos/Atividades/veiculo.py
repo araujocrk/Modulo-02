8cbacdbeca2aab224ab03574c4f2e36d72149eee
Ryan Araújo <167326341+araujocrk@users.noreply.github.com>
2024-11-06 15:54:01 -0300

    c. veiculo.py

---
+++ Programacao_Orientada_a_Objetos/Atividades/veiculo.py
@@ -0,0 +1,19 @@
+class Veiculo:
+    def __init__(self, placa, marca, modelo, ano, \
+    cor = 'Não especificada', proprietario = 'Não especificado', \
+    quilometragem = 0):
+        self.placa = placa
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.cor = cor
+        self.proprietario = proprietario
+        self.quilometragem = quilometragem
+
+    def __str__(self):
+        print()
+
+def main():
+
+if __name__ == '__main__':
+    main()
