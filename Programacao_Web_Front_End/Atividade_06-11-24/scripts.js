3ba7a5df08e3d308b4098bf924c0cd76df7ee063
araujocrk <ryansaopaulo2020@outlook.com>
2024-11-07 22:53:49 -0300

    f. Atividade_06-11-24

--- Programacao_Web_Front_End/Atividade_06-11-24/scripts.js
+++ Programacao_Web_Front_End/Atividade_06-11-24/scripts.js
@@ -6,9 +6,9 @@ function click (id, button) {
     return getid(id).addEventListener('click', button);
 }
 
-click ('buttonTextReverse', () => {
-    var text = getid('textReverse').value;
-    getid('resultReverse').innerHTML = 'Seu texto invertido:' + ' ' + text.split('').reverse('').join('');
+click ('buttonPadStart', () => {
+    var padStart = getid('padStart').value;
+    getid('resultPadStart').innerHTML = 'Seu número com 5 zeros a esquerda:' + ' ' + padStart.padStart(padStart.length + 5, '0');
 });
 
 click ('buttonOperations', () => {
@@ -29,4 +29,18 @@ click ('buttonCelsius', () => {
     var celsius = parseFloat(getid('temperatureCelsius').value);
     getid('resultCelsiusToFahrenheit').innerHTML = 'Temperatura em Fahrenheit:' + ' ' + 
     ((celsius * (9 / 5)) + 32) + ' ' + 'F°'
+});
+
+const date = new Date();
+getid('resultCurrentDate').innerHTML = date.toLocaleDateString('pt-BR');
+// getid('resultCurrentDate').innerHTML = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+
+function onKeyUp() {
+    var onKeyUp = getid('onKeyUp').value;
+    getid('resultOnKeyUp').innerHTML = onKeyUp;
+}
+
+click ('buttonTextReverse', () => {
+    var text = getid('textReverse').value;
+    getid('resultReverse').innerHTML = 'Seu texto invertido:' + ' ' + text.split('').reverse('').join('');
 });
