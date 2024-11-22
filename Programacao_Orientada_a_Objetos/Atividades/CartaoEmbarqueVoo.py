ff77c18064ddf1698a97f52ec4b642e76846a970
araujocrk <ryansaopaulo2020@outlook.com>
2024-11-21 23:36:06 -0300

    Update CartaoEmbarqueVoo.py

--- Programacao_Orientada_a_Objetos/Atividades/CartaoEmbarqueVoo.py
+++ Programacao_Orientada_a_Objetos/Atividades/CartaoEmbarqueVoo.py
@@ -1,6 +1,6 @@
 from datetime import *
 from random import *
-class CartaoEmbarque:#
+class CartaoEmbarque:
     # Assentos totais
     assentos = ['A1', 'B1', 'C1', 'D1', 'A2', 'B2', 'C2', 'D2', 'A3', 'B3', 'C3', 'D3', 'A4', 'B4', 'C4', 'D4', 'A5', 'B5', 'C5', 'D5', 'A6', 'B6', 'C6', 'D6', 'A7', 'B7', 'C7', 'D7', 'A8', 'B8', 'C8', 'D8', 'A9', 'B9', 'C9', 'D9', 'A10', 'B10', 'C10', 'D10']
     # Assentos ocupados
@@ -74,6 +74,7 @@ class CartaoEmbarque:#
             raise ValueError('Assento inválido.')
         
     def alterarAssento(self):
+        # Se o check-in foi realizado, remove o assento da lista de assentos ocupados e solicita ao usuário qual assento ele deseja e verifica se ele estaá disponível
         if self.checkIn == 'Realizado':
             CartaoEmbarque.assentosOcupados.remove(self.assento)
             assento = input('Para qual assento deseja mudar? ')
