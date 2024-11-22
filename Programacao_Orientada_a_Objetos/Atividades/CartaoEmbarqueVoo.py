b4a5e4cfa627188cfbed7ab2551173e6d59c2f94
araujocrk <ryansaopaulo2020@outlook.com>
2024-11-22 10:54:46 -0300

    Update CartaoEmbarqueVoo.py

--- Programacao_Orientada_a_Objetos/Atividades/CartaoEmbarqueVoo.py
+++ Programacao_Orientada_a_Objetos/Atividades/CartaoEmbarqueVoo.py
@@ -1,25 +1,30 @@
 from datetime import *
-from random import *
+import random
 class CartaoEmbarque:
     # Assentos totais
     assentos = ['A1', 'B1', 'C1', 'D1', 'A2', 'B2', 'C2', 'D2', 'A3', 'B3', 'C3', 'D3', 'A4', 'B4', 'C4', 'D4', 'A5', 'B5', 'C5', 'D5', 'A6', 'B6', 'C6', 'D6', 'A7', 'B7', 'C7', 'D7', 'A8', 'B8', 'C8', 'D8', 'A9', 'B9', 'C9', 'D9', 'A10', 'B10', 'C10', 'D10']
+    # Assentos disponíveis
+    assentosDisponiveis = ['A1', 'B1', 'C1', 'D1', 'A2', 'B2', 'C2', 'D2', 'A3', 'B3', 'C3', 'D3', 'A4', 'B4', 'C4', 'D4', 'A5', 'B5', 'C5', 'D5', 'A6', 'B6', 'C6', 'D6', 'A7', 'B7', 'C7', 'D7', 'A8', 'B8', 'C8', 'D8', 'A9', 'B9', 'C9', 'D9', 'A10', 'B10', 'C10', 'D10']
     # Assentos ocupados
     assentosOcupados = []
-    def __init__ (self, nome, numeroVoo, codigoDaReserva, data, horario, checkIn = None, assento = None):
+    def __init__ (self, nome, numeroVoo, codigoDaReserva, data, horario, checkIn = 'Não realizado', assento = 'Não informado'):
         self.nome = nome
         self.numeroVoo = numeroVoo
-        self.codigoDaReserva = codigoDaReserva
-        self.data = self.validarData(data)
-        self.horario = self.validarHorario(horario)
+        self.codigoDaReserva = self.validarCodigoDaReserva(codigoDaReserva)
+        self.dataHoraEmbarque = self.validarDataHora(data, horario)
         # Se o usuário não informar o check-in, será marcado como "não realizado"
-        if checkIn[0] == 'R':
+        if checkIn == 'Realizado':
             self.checkIn = 'Realizado'
+            if assento is None:
+                self.assento = self.assentoEstahDisponivel(random.choice(CartaoEmbarque.assentosDisponiveis))
+            else:
+                self.assento = self.assentoEstahDisponivel(assento)
         else:
             self.checkIn = 'Não realizado'
-        # Se o usuário informar um assento, ele será validado, se não, ele será definido como "sem assento"
-        self.assento = self.assentoEstahDisponivel(assento) if assento is not None else 'Sem assento'
+            self.assento = 'Não informado'
+        
     
-    def validarData(self, dataStr):
+    def validarDataHora(self, dataStr, horario):
         # Tenta converter a data no formato string em variáveis dia, mes e ano no formato inteiro, caso falhe, lança um ValueError
         try:
             dia = int(dataStr[:2])
@@ -27,20 +32,19 @@ class CartaoEmbarque:
             ano = int(dataStr[4:])
         except ValueError:
             raise ValueError('Data inválida! Digite números válidos no formato ddmmaaaa')
-        # Se a data for válida, retorna a data no formato inteiro
-        if date(ano, mes, dia):
-            return (dia * 1000000) + (mes * 10000) + ano
-    
-    def validarHorario(self, horario):
-        # Tenta converter o horário no formato string em variáveis hora e minuto no formato inteiro, caso falhe, lança um ValueError
+         # Tenta converter o horário no formato string em variáveis hora e minuto no formato inteiro, caso falhe, lança um ValueError
         try:
             hora = int(horario[:2])
             minuto = int(horario[2:])
         except ValueError:
             raise ValueError('Horário inválido! Digite números válidos no formato hhmm')
-        # Se o horário for válido, retorna o horário no formato inteiro
-        if time(hora, minuto):
-            return (hora * 100) + minuto
+        horaAtual = datetime.now()
+        horaEmbarque = datetime(ano, mes, dia, hora, minuto)
+        if horaEmbarque > horaAtual:
+            print('Horário válido para embarque.')
+            return horaEmbarque
+        else:
+            raise ValueError('Horário inválido! O horário informado deve ser posterior ao horário atual.')
     
     def validarCodigoDaReserva(self, codigoDaReserva):
         # Se o código da reserva for alfabetico e tiver 6 caracteres, retorna o código da reserva, se não, lança um ValueError
@@ -48,42 +52,45 @@ class CartaoEmbarque:
             return codigoDaReserva
         else:
             raise ValueError('Código da reserva inválido!')
-    
-    def realizarCheckIn(self):
-        # Verifica se o check-in já foi realizado, se não, ele será realizado e o assento será escolhido aleatoriamente
-        if self.checkIn == 'Não realizado':
-            self.checkIn = 'Realizado'
-            # Se o assento for nulo, ele será escolhido aleatoriamente
-            if self.assento == None:
-                self.assento = self.assentoEstahDisponivel(self, random.choice(CartaoEmbarque.assentos))
-        else:
-            print('Check=in já foi realizado.')
             
     def assentoEstahDisponivel(self, assento):
-        # Se o assento estiver na lista de assentos e não estiver ocupado, ele sera adicionado na lista de assentos ocupados e retorna o assento, se nao, lanca um ValueError
+        # Se o assento estiver na lista de assentos e não estiver ocupado, ele será removido da lista de assentos disponíveis
+        # e será adicionado na lista de assentos ocupados e retorna o assento, se nao, lanca um ValueError
         if assento in CartaoEmbarque.assentos:
-            if assento not in CartaoEmbarque.assentosOcupados:
+            if assento in CartaoEmbarque.assentosDisponiveis:
+                CartaoEmbarque.assentosDisponiveis.remove(assento)
                 CartaoEmbarque.assentosOcupados.append(assento)
+                print('Assento escolhido com sucesso!')
                 return assento
             # Se o assento estiver ocupado, ele será escolhido aleatoriamente
             else:
                 print('Este assento já está ocupado.')
                 print('Procurando assento disponível...')
-                return self.assentoEstahDisponivel(self, random.choice(CartaoEmbarque.assentos))
+                return self.assentoEstahDisponivel(random.choice(CartaoEmbarque.assentosDisponiveis))
         else:
             raise ValueError('Assento inválido.')
         
-    def alterarAssento(self):
+    def realizarCheckIn(self):
+        # Verifica se o check-in já foi realizado, se não, ele será realizado e o assento será escolhido aleatoriamente
+        if self.checkIn[0] == 'N':
+            self.checkIn = 'Realizado'
+            # Se o assento for nulo, ele será escolhido aleatoriamente
+            self.assento = self.assentoEstahDisponivel(random.choice(CartaoEmbarque.assentosDisponiveis))
+        else:
+            print('Check=in já foi realizado.')
+        
+    def alterarAssento(self, assento):
         # Se o check-in foi realizado, remove o assento da lista de assentos ocupados e solicita ao usuário qual assento ele deseja e verifica se ele estaá disponível
-        if self.checkIn == 'Realizado':
+        if self.checkIn[0] == 'R':
             CartaoEmbarque.assentosOcupados.remove(self.assento)
-            assento = input('Para qual assento deseja mudar? ')
-            self.assentoEstahDisponivel(self, assento)
+            #assento = input('Para qual assento deseja mudar? ')
+            self.assentoEstahDisponivel(assento)
         else:
             print('Check-in ainda não foi realizado.')
         
     def __str__(self):
-        return f'Nome: {self.nome}\nNúmero do Voo: {self.numeroVoo}\nCódigo da Reserva: {self.codigoDaReserva}\nData: {self.data}\nHorário: {self.horario}\nCheck-in: {self.checkIn}\nAssento: {self.assento}'
+        return f'Nome: {self.nome}\nNúmero do Voo: {self.numeroVoo}\nCódigo da Reserva: {self.codigoDaReserva} \
+        \nData e Hora do Embarque: {self.dataHoraEmbarque}\nCheck-in: {self.checkIn}\nAssento: {self.assento}'
     
 def atributosCartaoEmbarque():
     nome = input('Nome: ')
@@ -96,8 +103,22 @@ def atributosCartaoEmbarque():
     return nome, numeroVoo, codigoDaReserva, data, horario, checkIn, assento
         
 def main():
-    cartao1 = CartaoEmbarque(*atributosCartaoEmbarque())
+    #cartao1 = CartaoEmbarque(*atributosCartaoEmbarque())
+    #print(cartao1)
+    cartao1 = CartaoEmbarque('Ryan da Silva Araújo', '2311', 'AB12CD', '22112024', '1800', None, None)
+    cartao1.realizarCheckIn()
+    cartao1.alterarAssento('A7')
     print(cartao1)
+    cartao2 = CartaoEmbarque('Ramon da Silva Araújo', '2311', 'AB13CD', '22112024', '1800', None, None)
+    cartao2.realizarCheckIn()
+    cartao2.alterarAssento('B7')
+    print(cartao2)
+    cartao3 = CartaoEmbarque('Fernando Godoi', '2311', 'AB14CD', '22112024', '1800', 'Realizado', 'A8')
+    print(cartao3)
+    
+    
+    
+    
 if __name__ == '__main__':
     main()
         
