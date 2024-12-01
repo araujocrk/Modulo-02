00fda2b9ee40eb0b47231445246a074ce6b47833
araujocrk <ryansaopaulo2020@outlook.com>
2024-12-01 20:13:15 -0300

    add aula_26-11-24

---
+++ Programacao_Web_Front_End/aula_26-11-24/scripts.js
@@ -0,0 +1,39 @@
+function getById(id) {
+    return document.getElementById(id);
+}
+
+let alterarFundo = getById('alterarFundo');
+alterarFundo.addEventListener('click', () => {
+    getById('caixaFundo').style.backgroundColor = 'blue';
+});
+
+
+let fontFamilyOriginal;
+let fontSizeOriginal;
+let mudou = false;
+
+let alterarFonte = getById('alterarFonte');
+alterarFonte.addEventListener('click', () => {
+    let texto = getById('texto');
+
+    if (!mudou) {
+        fontFamilyOriginal = texto.style.fontFamily;
+        fontSizeOriginal = texto.style.fontSize;
+
+        texto.style.fontSize = '40px';
+        texto.style.fontFamily = 'Arial';
+
+        mudou = true;
+    } 
+    else {
+        texto.style.fontSize = fontSizeOriginal;
+        texto.style.fontFamily = fontFamilyOriginal;
+        mudou = false;
+    }
+});
+
+let corFundo = getById('corFundo');
+
+corFundo.addEventListener('input', () => {
+    document.body.style.backgroundColor = corFundo.value;
+});
