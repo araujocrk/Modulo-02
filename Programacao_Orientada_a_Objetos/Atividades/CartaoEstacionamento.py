ff51f443d5477b39c46982d4e26ae4d6d3e4790f
araujocrk <ryansaopaulo2020@outlook.com>
2024-12-02 21:36:47 -0300

    Update CartaoEstacionamento.py

--- Programacao_Orientada_a_Objetos/Atividades/CartaoEstacionamento.py
+++ Programacao_Orientada_a_Objetos/Atividades/CartaoEstacionamento.py
@@ -1,78 +1,78 @@
 import random
 import string
-from datetime import *
+from datetime import datetime
+
 class CartaoEstacionamento:
     numCartoesAtivos = []
-    def gerarCartao():
-        return ''.join(random.choices(string.ascii_letters + string.digits, k=8))
-    
-    def gerarDataHora():
-        return datetime.now()
-    
-    def __init__(self):
-        self.__numCartao = self.cartaoValido(CartaoEstacionamento.gerarCartao())
-        self.dataHoraEntrada = CartaoEstacionamento.gerarDataHora()
+    def __init__(self, dataHoraEntrada):
+        self.dataHoraEntrada = self.validarDataHora(dataHoraEntrada)
+        self.__numCartao = self.gerarNumCartao()
         self.status = 'Ativo'
-        self.dataHoraSaida = 'Sem saída'
-        self.__valorTotal = 0
-    
-    @property
-    def numCartao(self):
-        return self.__numCartao
-    
-    @property
-    def valorTotal(self):
-        return self.__valorTotal
-    
-    @valorTotal.setter
-    def valorTotal(self, valor):
-        self.__valorTotal = valor
-        
-    def cartaoValido(self, numCartao):
+        self.dataHoraSaida = 'Não informado'
+        self.__valor = 0
+
+    def validarDataHora(self, dataHora):
+        dataHora = datetime.strptime(dataHora, '%d/%m/%Y %H:%M')
+        return dataHora.strftime('%d/%m/%Y %H:%M')
+
+    def gerarNumCartao(self):
+        numCartao = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
         if numCartao not in CartaoEstacionamento.numCartoesAtivos:
             CartaoEstacionamento.numCartoesAtivos.append(numCartao)
             return numCartao
         else:
-            return self.cartaoValido(CartaoEstacionamento.gerarCartao())
-        
-    def finalizarSaida(self):
-        dataHoraSaida = CartaoEstacionamento.gerarDataHora()
-        if dataHoraSaida < self.dataHoraEntrada:
-            raise ValueError('Data ou horário de saída inválidos')
-        else:
-            self.dataHoraSaida = dataHoraSaida
-            self.status = 'Finalizado'
-            CartaoEstacionamento.numCartoesAtivos.remove(self.numCartao)
-            self.calcularValorTotal(dataHoraSaida)
-    
-    def calcularValorTotal(self, dataHoraAtual):
-        permanencia = dataHoraAtual - self.dataHoraEntrada
+            return self.gerarNumCartao()
+
+    @property
+    def numCartao(self):
+        return self.__numCartao
+
+    def calcularValor(self):
+        dataHoraEntrada = datetime.strptime(self.dataHoraEntrada, '%d/%m/%Y %H:%M')
+        permanencia = datetime.now() - dataHoraEntrada
         minutos = permanencia.total_seconds() // 60
         if minutos <= 120:
-            self.valorTotal = 8.0
-            print(self.valorTotal)
+            return 8.0
         else:
             excesso = minutos - 120
             valor_excesso = (excesso // 15) * 0.50
-            self.valorTotal = 8.0 + valor_excesso
-            print(self.valorTotal)
-            
+            return 8.0 + valor_excesso
+
+    def verValor(self):
+        return f'R${self.calcularValor():.2f}'
+
+    @property
+    def valor(self):
+        return self.__valor
+
+    @valor.setter
+    def valor(self, valor):
+        self.__valor = valor
+
+    def finalizarSaida(self):
+        dataHoraSaida = datetime.today()
+        dataHoraEntrada = datetime.strptime(self.dataHoraEntrada, '%d/%m/%Y %H:%M')
+        if dataHoraSaida < dataHoraEntrada:
+            raise ValueError('Data ou horário de saída inválidos')
+        else:
+            self.status = 'Finalizado'
+            self.dataHoraSaida = dataHoraSaida.strftime('%d/%m/%Y %H:%M')
+            CartaoEstacionamento.numCartoesAtivos.remove(self.numCartao)
+            self.valor = self.calcularValor()
+            print('Saída confirmada. Até a proxima!')
+
     def __str__(self):
-        entrada_formatada = self.dataHoraEntrada.strftime('%d-%m-%Y %H:%M')
-        saida_formatada = (
-        self.dataHoraSaida.strftime('%d-%m-%Y %H:%M') 
-        if isinstance(self.dataHoraSaida, datetime) 
-        else self.dataHoraSaida
-    )
-        return f'Cartão: {self.numCartao}\nEntrada: {entrada_formatada}\nSaida: {saida_formatada}\nStatus: {self.status}\nValor total: {self.valorTotal}'
-    
+        return f'Cartão: {self.numCartao}\nEntrada: {self.dataHoraEntrada}\nSaida: {self.dataHoraSaida}\nStatus: {self.status}\nValor total: R${self.valor:.2f}'
+
 def main():
-    cartao1 = CartaoEstacionamento()
+    cartao1 =  CartaoEstacionamento(input('Digite a data e hora de entrada no formato dd/mm/aaaa hh:mm: '))
     cartao1.finalizarSaida()
-    cartao2 = CartaoEstacionamento()
-    cartao2.calcularValorTotal(CartaoEstacionamento.gerarDataHora())
-    cartao3 = CartaoEstacionamento()
+    print(cartao1)
+    cartao2 = CartaoEstacionamento(input('Digite a data e hora de entrada no formato dd/mm/aaaa hh:mm: '))
+    print(cartao2.verValor())
+    cartao2.finalizarSaida()
+    cartao3 = CartaoEstacionamento(input('Digite a data e hora de entrada no formato dd/mm/aaaa hh:mm: '))
     print(cartao3)
-    
+
 if __name__ == '__main__':
     main()
