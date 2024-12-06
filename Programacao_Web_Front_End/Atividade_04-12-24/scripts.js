e8aacd43fe201c957ee818fc7536f9476276ae72
araujocrk <ryansaopaulo2020@outlook.com>
2024-12-06 12:06:16 -0300

    f. atividade_04-12-24

--- Programacao_Web_Front_End/Atividade_04-12-24/scripts.js
+++ Programacao_Web_Front_End/Atividade_04-12-24/scripts.js
@@ -17,22 +17,45 @@ inputTextUpper.addEventListener('keyup', () => {
 });
 
 let selectEmotions = getById('selectEmotions');
-selectEmotions.addEventListener('change', () =>{
+selectEmotions.addEventListener('change', () => {
     let imgEmotions = getById('imgEmotions');
-    imgEmotions.innerHTML = selectEmotions.value;
     let emotion = selectEmotions.value
     switch (emotion) {
-        case emotion == 'alegre':
-            imgEmotions.innerHTML = selectEmotions.value;
+        case 'alegre':
+            imgEmotions.src = 'imagens/alegria.png';
             break;
     
-        case (emotion == 'triste'):
-            imgEmotions.innerHTML = selectEmotions.value;
+        case 'triste':
+            imgEmotions.src = 'imagens/tristeza.png';
             break;
         
-        case (emotion == 'raiva'):
-            imgEmotions.innerHTML = selectEmotions.value;
+        case 'raiva':
+            imgEmotions.src = 'imagens/raiva.png';
             break;
     }
 });
 
+let inputChangeColor = getById('inputChangeColor');
+let backgroundColorDefault = inputChangeColor.style.backgroundColor;
+inputChangeColor.addEventListener('focus', () => {
+    inputChangeColor.style.backgroundColor = 'yellow';
+});
+
+inputChangeColor.addEventListener('blur', () => {
+    inputChangeColor.style.backgroundColor = backgroundColorDefault;
+});
+
+let inputPassword = getById('inputPassword');
+let tamanhoPassword;
+inputPassword.addEventListener('keyup', () => {
+    let password = inputPassword.value;
+    let passwordWithoutSpaces = password.trim();
+    tamanhoPassword = parseInt(passwordWithoutSpaces.length);
+});
+
+inputPassword.addEventListener('blur', () => {
+    let errorPassword = getById('errorPassword');
+    if (tamanhoPassword < 8) {
+        errorPassword.innerHTML = 'Erro: Sua senha tem menos de 8 caracteres';
+    }
+});
