a549926e1118c1fa1d44b23b684237e093c7321f
Ryan Araújo <167326341+araujocrk@users.noreply.github.com>
2025-01-23 17:21:06 -0300

    Update create-ads.sql

--- Engenharia_de_Software/Aventuras_do_Saber/teste-bd/create-ads.sql
+++ Engenharia_de_Software/Aventuras_do_Saber/teste-bd/create-ads.sql
@@ -6,8 +6,9 @@ DROP TABLE IF EXISTS professor;
 DROP TABLE IF EXISTS aluno;
 DROP TABLE IF EXISTS disciplina_aluno;
 
-DROP SCHEMA IF EXISTS ads-bd;
-SET search_path TO ads-bd;
+DROP SCHEMA IF EXISTS ads;
+CREATE SCHEMA ads;
+SET search_path TO ads;
 
 CREATE TABLE escola (
 	id_escola NUMERIC(6,0) PRIMARY KEY,
@@ -24,7 +25,7 @@ CREATE TABLE diretor (
 	telefone_diretor VARCHAR(11) NOT NULL,
 	id_escola NUMERIC(6,0) NOT NULL,
 	CONSTRAINT diretor_escola_fk FOREIGN KEY (id_escola) REFERENCES escola(id_escola) ON DELETE CASCADE,
-	UNIQUE id_escola
+	UNIQUE (id_escola)
 );
 
 CREATE TABLE turma (
@@ -33,7 +34,23 @@ CREATE TABLE turma (
 	ano_letivo NUMERIC(4) NOT NULL,
 	id_escola NUMERIC(6,0) NOT NULL,
 	CONSTRAINT turma_escola_fk FOREIGN KEY (id_escola) REFERENCES escola(id_escola) ON DELETE CASCADE,
-	UNIQUE id_escola
+	UNIQUE (id_escola) 
+);
+
+CREATE TABLE professor (
+	id_professor NUMERIC(6,0) PRIMARY KEY,
+	nome_professor VARCHAR(40) NOT NULL,
+	cpf_professor VARCHAR(11) NOT NULL,
+	email_professor VARCHAR(40) NOT NULL,
+	telefone_professor VARCHAR(11) NOT NULL
+);
+
+CREATE TABLE aluno (
+	id_aluno NUMERIC(6,0) PRIMARY KEY,
+	nome_aluno VARCHAR(40) NOT NULL,
+	data_nasc_aluno DATE NOT NULL,
+	nome_responsavel VARCHAR(40) NOT NULL,
+	telefone_responsavel VARCHAR(11) NOT NULL
 );
 
 CREATE TABLE disciplina (
@@ -42,13 +59,8 @@ CREATE TABLE disciplina (
 	id_turma NUMERIC(6,0) NOT NULL,
 	id_professor NUMERIC(6,0) NOT NULL,
 	id_aluno NUMERIC(6,0) NOT NULL,
-	CONSTRAINT 
+	CONSTRAINT disciplina_turma_fk FOREIGN KEY (id_turma) REFERENCES turma(id_turma) ON DELETE CASCADE,
+	CONSTRAINT disciplina_professor_fk FOREIGN KEY (id_professor) REFERENCES professor(id_professor) ON DELETE CASCADE,
+	CONSTRAINT disciplina_aluno_fk FOREIGN KEY (id_turma) REFERENCES aluno(id_aluno) ON DELETE CASCADE,
+	UNIQUE (id_turma, id_professor, id_aluno)
 );
-
-CREATE TABLE professor (
-	id_professor NUMERIC(6,0) PRIMARY KEY,
-	nome_professor VARCHAR(40) NOT NULL,
-	cpf_professor VARCHAR(11) NOT NULL,
-	email_professor VARCHAR(40) NOT NULL,
-	telefone_professor VARCHAR(11) NOT NULL,
-)
