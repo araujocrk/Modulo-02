e3107eab2077c6cb2c77f01b3b238b4b02e3fa5b
Ryan Araújo <167326341+araujocrk@users.noreply.github.com>
2025-01-29 15:39:08 -0300

    Add files via upload

---
+++ Programacao_Orientada_a_Objetos/Atividades/test_contaCorrente-Poupanca.py
@@ -0,0 +1,8 @@
+import pytest #Provavelmente precisa instalar via terminal
+from contaCorrente-Poupanca.py import #metodos
+
+def test_metodo():
+    argumento_de_teste = 'aqui'
+    resultado_esperado = 'aqui'
+    resultado = 'metodo(argumento_de_teste)'
+    
