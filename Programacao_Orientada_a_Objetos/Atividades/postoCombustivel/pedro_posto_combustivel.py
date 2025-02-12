8b3b8a8a3c2dc3aff691ee0f0f7a82c6e5a640e6
araujocrk <ryansaopaulo2020@outlook.com>
2025-02-12 00:05:46 -0300

    u. provaPOO

--- Programacao_Orientada_a_Objetos/Atividades/postoCombustivel/pedro_posto_combustivel.py
+++ Programacao_Orientada_a_Objetos/Atividades/postoCombustivel/pedro_posto_combustivel.py
@@ -1,3 +1,54 @@
+class PostoDeCombustivel:
+    def __init__(self, nome):
+        self.nome = nome
+        self.bombas = []
+        
+    # Adicionar bomba de combustivel
+    def adicionar_bomba(self, bomba):
+        # Recebe bomba e verifica se ela é do tipo BombaDeCombustivel
+        if isinstance(bomba, BombaDeCombustivel):
+            self.bombas.append(bomba)
+        else:
+            raise TypeError('Bomba inválida. A bomba deve ser do tipo BombaDeCombustivel.')
+
+    def listar_bombas(self):
+        # Para cada item na lista de bombas, imprime o identificador da bomba
+        return [f"Bomba: {b.identificador}" for b in self.bombas]
+   
+class BombaDeCombustivel:
+    # Contador de bombas
+    contadorId = 0
+    def __init__(self):
+        # Garante que cada Bomba de Combustível tenha seu próprio ID / auto-incremento
+        BombaDeCombustivel.contadorId += 1
+        # Recebe um identificador
+        self.identificador = BombaDeCombustivel.contadorId
+        self.combustivel = None
+        
+    def associar_combustivel(self, combustivel):
+        # Recebe combustivel e verifica se ele é do tipo Combustivel
+        if isinstance(combustivel, Combustivel): # Testar se é apenas a superclasse ou precisa das subclasses
+            self.combustivel = combustivel
+        else:
+            raise TypeError('Combustível inválido. O combustível deve ser do tipo Combustivel.')
+
+    def abastecer(self, qtd_litros):
+        if self.combustivel is None:
+            raise ValueError("Bomba sem combustível associado.")
+        total_pagar = self.combustivel.calcular_valor(qtd_litros)
+        return Abastecimento(self, qtd_litros, total_pagar)
+     
+class Abastecimento:
+    def __init__(self, bomba, qtd_litros, valor_total):
+        self.bomba = bomba
+        self.qtd_litros = qtd_litros
+        self.valor_total = valor_total
+
+    def __str__(self):
+        tipo_combustivel = type(self.bomba.combustivel).__name__
+        detalhes = "Aditivada" if isinstance(self.bomba.combustivel, Gasolina) and self.bomba.combustivel.aditivada else self.bomba.combustivel.origem
+        return f"Bomba {self.bomba.identificador} ({tipo_combustivel} - {detalhes}): {self.qtd_litros} litros -> R$ {self.valor_total:.2f}"     
+
 # Superclasse
 class Combustivel:
     def __init__(self, nome, preco_por_litro):
@@ -39,40 +90,3 @@ class Etanol(Combustivel):
         self.lista_origem = ['cana_de_açucar', 'milho']
         # Recebe origem do etanol e verifica se existe na lista de origens
         self.origem = origem
-
-class BombaDeCombustivel:
-    def __init__(self, identificador):
-        self.identificador = identificador
-        self.combustivel = None
-
-    def associar_combustivel(self, combustivel):
-        self.combustivel = combustivel
-
-    def abastecer(self, qtd_litros):
-        if self.combustivel is None:
-            raise ValueError("Bomba sem combustível associado.")
-        total_pagar = self.combustivel.calcular_valor(qtd_litros)
-        return Abastecimento(self, qtd_litros, total_pagar)
-
-class Abastecimento:
-    def __init__(self, bomba, qtd_litros, valor_total):
-        self.bomba = bomba
-        self.qtd_litros = qtd_litros
-        self.valor_total = valor_total
-
-    def __str__(self):
-        tipo_combustivel = type(self.bomba.combustivel).__name__
-        detalhes = "Aditivada" if isinstance(self.bomba.combustivel, Gasolina) and self.bomba.combustivel.aditivada else self.bomba.combustivel.origem
-        return f"Bomba {self.bomba.identificador} ({tipo_combustivel} - {detalhes}): {self.qtd_litros} litros -> R$ {self.valor_total:.2f}"
-
-class PostoDeCombustivel:
-    def __init__(self, nome):
-        self.nome = nome
-        self.bombas = []
-    # Adiciona boma de combustivel
-    def adicionar_bomba(self, bomba):
-        # Recebe bomba e verifica se ela é do tipo BombaDeCombustivel
-        self.bombas.append(bomba)
-
-    def listar_bombas(self):
-        return [f"Bomba {b.identificador}" for b in self.bombas]
